4a038605ce89299d59a422cf0cb7927ca4d67d34
Sam Hay <snh68@yahoo.com>
2023-06-28 23:16:41 -0700

    set up backend

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { Post } from "../post.model";
+import { PostsService } from "../posts.service";
+import { Subscription } from "rxjs";
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+
+})
+
+export class PostListComponent implements OnInit, OnDestroy {
+    posts : Post[] = []
+    private postsSub: Subscription = new Subscription();
+
+    constructor(private postsService: PostsService ) {
+    }
+
+    ngOnInit() {
+        this.postsService.getPosts()
+        this.postsService.getPostUpdateListener()
+        .subscribe(
+            (posts: Post[]) => { 
+                this.posts = posts
+            }
+        )
+    }
+
+    ngOnDestroy() {
+        this.postsSub.unsubscribe()
+    } 
+}
